4bb0fc37a742fd8fa6a30427d4ed87a4d94568be
Aaron Levesque <99132087+aleve2@users.noreply.github.com>
2023-12-01 14:11:54 -0500

    Update btree.cpp

--- btree.cpp
+++ btree.cpp
@@ -13,7 +13,18 @@ void BTree::insertNonFull(BTreeNode* node, const std::string& key) {
 }
 
 void BTree::insert(const std::string& key) {
-
+    if (root == nullptr) {
+        root = new BTreeNode(true);
+        root->keys.push_back(key);
+        root->counts.push_back(1);
+    } else {
+        if (root->keys.size() == (2 * t - 1)) {
+            BTreeNode* new_root = new BTreeNode(false);
+            new_root->children.push_back(root);
+            splitChild(new_root, 0);
+            root = new_root;
+        }
+    }
 }
 
 void BTree::search(const std::string& key) {
