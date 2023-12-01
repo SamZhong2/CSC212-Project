5c8ebc5e44cc38b46faa071b9f2bec88bb7cdd63
Aaron Levesque <99132087+aleve2@users.noreply.github.com>
2023-12-01 14:25:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include "btree.h"
 
 int main() {
-    
+    BTree btree;
+
+    std::ifstream inputFile("test_1.txt");
+    if (!inputFile.is_open()) {
+        std::cerr << "Error opening input file." << std::endl;
+        return 1;
+    }
+
+    std::string word;
+    while (inputFile >> word) {
+        btree.insert(word);
+    }
+
+    inputFile.close();
+
+    std::string searchWord;
+    std::cout << "Enter a word to search: ";
+    std::cin >> searchWord;
+    btree.search(searchWord);
+
+    btree.generateDotFile("btree.dot");
+
+    return 0;
 }
