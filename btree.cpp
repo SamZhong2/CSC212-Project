017ed35a69ac0294e637eee4d7d4b1c9bf991c85
Aaron Levesque <99132087+aleve2@users.noreply.github.com>
2023-12-01 14:21:37 -0500

    Update btree.cpp

--- btree.cpp
+++ btree.cpp
@@ -11,7 +11,27 @@ void BTree::splitChild(BTreeNode* parent, int index) {
 }
 
 void BTree::insertNonFull(BTreeNode* node, const std::string& key) {
-    
+    int i = node->keys.size() - 1;
+
+    if (node->is_leaf) {
+        while (i >= 0 && key < node->keys[i]) {
+            i--;
+        }
+        node->keys.insert(node->keys.begin() + i + 1, key);
+        node->counts.insert(node->counts.begin() + i + 1, 1);
+    } else {
+        while (i >= 0 && key < node->keys[i]) {
+            i--;
+        }
+
+        i++;
+        if (node->children[i]->keys.size() == (2 * t - 1)) {
+            splitChild(node, i);
+            if (key > node->keys[i])
+                i++;
+        }
+        insertNonFull(node->children[i], key);
+    }
 }
 
 void BTree::insert(const std::string& key) {
@@ -26,6 +46,7 @@ void BTree::insert(const std::string& key) {
             splitChild(new_root, 0);
             root = new_root;
         }
+        insertNonFull(root, key);
     }
 }
 
