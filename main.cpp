419c5874255c16c79fa15c6e9a2aa32452ef5149
Sam Zhong <118559877+SamZhong2@users.noreply.github.com>
2023-12-02 14:09:40 -0500

    Update user input and some error detection

--- main.cpp
+++ main.cpp
@@ -4,25 +4,38 @@
 #include <string>
 #include "btree.h"
 
-// Inputs...       2           3
-// ./btree [input_file.txt] [degree]
 
-int main(int argc, char*argv[]) {
+int main() {
+    std::string filename;
+    int degree;
 
     // Initialize the BTree...
-    BTree btree;
-    if (argc > 2) {
-        btree = BTree(std::stoi(argv[2]));
+    std::cout << "Enter input file name: ";
+    std::cin >> filename;
+    std::cout << "-----------------------" << std::endl;
+    
+    while (true) {
+        std::cout << "Enter degree or order: ";
+        
+        // Check if the user input is of integer type
+        if (std::cin >> degree) {
+            std::cout << "-----------------------" << std::endl;
+            break; // Exit the loop if input is a valid integer
+        } else {
+            std::cin.clear(); // Clear the error flag
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
+            std::cout << "Invalid input. Please enter an integer." << std::endl;
+            std::cout << "-----------------------" << std::endl;
+        }
     }
-    // Get the filename...
-    std::string filename = "test_1.txt";
-    if (argc > 1) {
-        filename = argv[1];
-    }
+    BTree btree(degree);
+    
+    
 
     std::ifstream inputFile(filename);
     if (!inputFile.is_open()) {
-        std::cerr << "Error opening input file." << std::endl;
+        std::cerr << "Invvalid file" << std::endl;
+        std::cout << "-----------------------" << std::endl;
         return 1;
     }
 
@@ -36,8 +49,10 @@ int main(int argc, char*argv[]) {
     std::string searchWord;
     std::cout << "Enter a word to search: ";
     std::cin >> searchWord;
+    std::cout << "-----------------------" << std::endl;
     int count = btree.search(searchWord);
     std::cout << "Found the word \"" << searchWord << "\" a total of " << count << " times." << std::endl;
+    std::cout << "-----------------------" << std::endl;
 
     btree.generateDotFile("btree.dot");
 
