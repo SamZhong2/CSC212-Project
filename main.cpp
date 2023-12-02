52200b48fc7e524316cf42c76068079106871d66
Ben Uthoff <81537940+benuthoff@users.noreply.github.com>
2023-12-02 16:24:11 -0500

    Fixes to main.cpp
    - Fixed comments and grammar.
    - Brought back CLI arguments, but made them optional.

--- main.cpp
+++ main.cpp
@@ -4,46 +4,59 @@
 #include <string>
 #include "btree.h"
 
+// Two methods of input.
+// If no command line arguments, use cin.
+// Inputs...   argv[1]       argv[2]
+// ./btree [input_file.txt] [degree]
 
-int main() {
+int main(int argc, char*argv[]) {
     std::string filename;
     int degree;
 
-    // Initialize the BTree...
-    std::cout << "Enter input file name: ";
-    std::cin >> filename;
-    std::cout << "-----------------------" << std::endl;
-    
-    while (true) {
-        std::cout << "Enter degree or order: ";
-        
-        // Check if the user input is of integer type
-        if (std::cin >> degree) {
-            std::cout << "-----------------------" << std::endl;
-            break; // Exit the loop if input is a valid integer
-        } else {
-            std::cin.clear(); // Clear the error flag
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
-            std::cout << "Invalid input. Please enter an integer." << std::endl;
-            std::cout << "-----------------------" << std::endl;
-        }
+    // Get the file name...
+    if (argc < 2) {
+        std::cout << "Enter input file name: ";
+        std::cin >> filename;
+        std::cout << "-----------------------" << std::endl;
+    } else {
+        filename = argv[1];
     }
+    
+    // Get the degree of the tree...
+    if (argc < 3) {
+        while (true) {
+            std::cout << "Enter degree or order: ";
+            // Check if the user input is of integer type
+            if (std::cin >> degree) {
+                std::cout << "-----------------------" << std::endl;
+                break; // Exit the loop if input is a valid integer
+            } else {
+                std::cin.clear(); // Clear the error flag
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
+                std::cout << "Invalid input. Please enter an integer." << std::endl;
+                std::cout << "-----------------------" << std::endl;
+            }
+        }
+    } else {
+        degree = std::stoi(argv[2]);
+    }
+
+    // Initialize the BTree...
     BTree btree(degree);
     
-    
-
+    // Open the file...
     std::ifstream inputFile(filename);
     if (!inputFile.is_open()) {
-        std::cerr << "Invvalid file" << std::endl;
+        std::cerr << "Invalid file" << std::endl;
         std::cout << "-----------------------" << std::endl;
         return 1;
     }
 
+    // Adds words in file to the BTree, then close...
     std::string word;
     while (inputFile >> word) {
         btree.insert(word);
     }
-
     inputFile.close();
 
     std::string searchWord;
@@ -51,7 +64,7 @@ int main() {
     std::cin >> searchWord;
     std::cout << "-----------------------" << std::endl;
     int count = btree.search(searchWord);
-    std::cout << "Found the word \"" << searchWord << "\" a total of " << count << " times." << std::endl;
+    std::cout << "Found the word \"" << searchWord << "\" a total of " << count << " time(s)." << std::endl;
     std::cout << "-----------------------" << std::endl;
 
     btree.generateDotFile("btree.dot");
